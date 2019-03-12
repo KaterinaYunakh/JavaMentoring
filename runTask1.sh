c561266d7383a89788d8a4abf064d79b40051857
Katerina Yunakh <ms.bonevich@mail.ru>
2019-03-12 21:02:42 +0100

    Updated version: package, scripts, generic method

--- runTask1.sh
+++ runTask1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir -p src/main/java/classes
+
 "$JAVA_HOME\bin\javac" -d src/main/java/classes -cp src/main/java src/main/java/com/katerina/Task1/Task1.java
 "$JAVA_HOME\bin\java" -cp src/main/java/classes Task1
 read -p "Press [Enter] key to continue"
